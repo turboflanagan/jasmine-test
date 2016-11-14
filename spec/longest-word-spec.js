66ff6b43d8a28234df322cd0ee5ad1b37459c709
turboflanagan <turboflanagan@gmail.com>
2016-11-14 15:38:42 -0500

    init commit

--- spec/longest-word-spec.js
+++ spec/longest-word-spec.js
@@ -6,4 +6,8 @@ describe("longest-word", function() {
         expect(longWordArray).toEqual(["The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]);
     });
 
+    it("should get the length of the longest word in the array", function() {
+        var longWordArray = findLongestWord("The quick brown fox jumped over the lazy dog");
+        expect(longWordArray).toEqual(6);
+    });
 });
--- src/longest-word.js
+++ src/longest-word.js
@@ -2,8 +2,14 @@ var findLongestWord = function(str) {
     var str = str;
     var sentence = str.split(" ");
     console.log("sentence = " + sentence);
-    console.log("str = " + str);
-    return sentence;
+    var wordLength = 0;
+    for (i = 0; i < sentence.length; i++) {
+        if(sentence[i].length > wordLength) {
+            wordLength = sentence[i].length;
+        };
+    };
+    console.log("wordLength = " + wordLength);
+    return wordLength;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
